74f67bcf9386c4446e669d01f8dcf22b81c1af0c
NuS2k <ngoduong543@gmail.com>
2022-08-06 04:39:21 +0000

    fix test/auth

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+
+class RegisterController extends Controller
+{
+    use RegistersUsers;
+
+    protected $redirectTo = '/login';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'username' => ['required', 'string', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
+
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'username' => $data['username'],
+            'password' => Hash::make($data['password']),
+            'type' => User::TYPES['student'],
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        if ($response = $this->registered($request, $user)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 201)
+            : redirect($this->redirectPath())
+                ->with(
+                    'Account created. Please check your mailbox for verification email.'
+                );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,14 @@ use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', function () {
+    return redirect('/home');
+});
 
 Route::name('admin.')->prefix('admin')->group(function () {
     Route::name('user.')->prefix('user')->group(function () {
@@ -18,3 +25,5 @@ Route::name('admin.')->prefix('admin')->group(function () {
     Route::resource('product', ProductController::class);
     Route::resource('category', CategoryController::class);
 });
+
+Auth::routes(['verify' => true]);
