dffb838d7d709ce127b6ae85cbd374d9e5aaa06c
NuS2k <99632428+NuS2k@users.noreply.github.com>
2022-08-10 13:32:43 +0700

    Revert "Permission_group_management"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,13 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->app->register(
-            RespositoryServiceProvider::class
-        );
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
         //
