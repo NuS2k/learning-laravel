064f5b945877a261b0275d07def0c02774209a04
NuS2k <ngoduong543@gmail.com>
2022-08-12 14:18:34 +0000

    make PermissionMNG

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -3,42 +3,73 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Repositories\Admin\Permission\PermissionRepositoryInterface as PermissionRepository;
+use App\Repositories\Admin\PermissionGroup\PermissionGroupRepositoryInterface as PermissionGroupRepository;
 
 class PermissionController extends Controller
 {
+    protected $permissionRepository;
+    protected $permissionGroupRepository;
+
+    public function __construct(PermissionRepository $permissionRepository, PermissionGroupRepository $permissionGroupRepository)
+    {
+        $this->permissionRepository = $permissionRepository;
+        $this->permissionGroupRepository = $permissionGroupRepository;
+    }
+
     public function index()
     {
-        return view('admin.permission.index');
+        return view('admin.permission.index', [
+            'permissions' => $this->permissionRepository->paginate(),
+        ]);
     }
 
     public function create()
     {
-        return view('admin.permission.create');
+        return view('admin.permission.form', [
+            'permissionGroups' => $this->permissionGroupRepository->getAll(),
+        ]);
     }
 
     public function store(Request $request)
     {
-        //
+        $this->permissionRepository->save($request->validated());
+
+        return redirect()->route('admin.permission.index');
     }
 
     public function show($id)
     {
-        //
+        $permission = $this->permissionRepository->findById($id);
+
+        if (!$permission) {
+            abort(404);
+        }
+
+        return view('admin.permission.show', [
+            'permission' => $permission,
+        ]);
     }
 
     public function edit($id)
     {
-        //
+        return view('admin.permission.form', [
+            'permission' => $this->permissionRepository->findById($id),
+            'permissionGroups' => $this->permissionGroupRepository->getAll(),
+        ]);
     }
 
     public function update(Request $request, $id)
     {
-        //
+        $this->permissionRepository->save($request->validated(), ['id' => $id]);
+
+        return redirect()->route('admin.permission.index');
     }
 
     public function destroy($id)
     {
-        //
+        $this->permissionRepository->deleteById($id);
+
+        return redirect()->route('admin.permission.index');
     }
 }
---
+++ app/Http/Requests/Admin/PermissionRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class PermissionRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => [
+                'required',
+                Rule::unique('permissions')->ignore($this->permission)
+            ],
+
+            'key' => [
+                'required',
+                Rule::unique('permissions')->ignore($this->permission)
+            ],
+
+            'permission_group_id' => [
+                'required',
+                'numeric',
+                'size: 0',
+                Rule::exists('permissions', 'id'),
+            ],
+        ];
+    }
+}
